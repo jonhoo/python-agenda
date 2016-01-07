41e4ac70b4c51762bd0c109e886a1573986e3064
Jon Gjengset <jon@thesquareplanet.com>
2016-01-07 11:37:49 -0500

    setup.py

---
+++ setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='agenda',
+    version='1.0.0.dev0',
+    license='MIT',
+
+    description='Module for pretty task logging',
+    long_description=long_description,
+
+    url='https://github.com/jonhoo/python-agenda',
+    author='Jon Gjengset',
+    author_email='jon@thesquareplanet.com',
+
+    classifiers=[
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Developers',
+        'Topic :: System :: Logging',
+        'Environment :: Console',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    keywords='logging console pretty-print',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+
+    install_requires=['future', 'termcolor'],
+)
