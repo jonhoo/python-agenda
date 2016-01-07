d5986bd1badecbe0ebb83b117e4eebaa4a28c597
Jon Gjengset <jon@thesquareplanet.com>
2016-01-07 12:31:46 -0500

    Add README in Manifest

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='agenda',
-    version='1.0.0',
+    version='1.0.1',
     license='MIT',
 
     description='Module for pretty task logging',
